41a80a79ef93ad725ff34f28d2c5887f1203108a
Tobias Oberdoerfer <tobiaso@ethz.ch>
2023-03-16 10:28:57 +0100

    Simple backend replaying weatherBit data for now + fetching & caching of data in frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useState } from 'react';
 import BarComponent from '../components/BarComponent';
+import { useFetch } from '../hooks/useFetch';
 
 var ogData = [
   {year: 1980, efficiency: 24.3, sales: 8949000},
@@ -34,13 +36,78 @@ var ogData = [
   {year: 2017, efficiency: 39.4, sales: 6081000},
 ]
 
+//const weatherUrl = "https://api.weatherbit.io/v2.0/forecast/daily?city=Lausanne&days=7&key=de7baa6dfedd4ee8b140662b5298b160"
+const weatherUrl = "http://localhost:8000/testData.json"
+
 function Home() {
+	const [city, setCity] = useState('Lausanne');
+
+	const url = city && `http://localhost:8000/testData.json?query=${city}`;
+
+	const { status, data, error } = useFetch(url);
+
+  const weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ]
+
+  const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const query = e.target.search.value;
+		if (query) {
+			setCity(query);
+			e.target.search.value = '';
+		}
+	};
+
+  const weatherbitToTemperatures = (weatherBitData) => {
+    // Converts the weatherbit data to the following format
+    // array of {'y': temperature, 'x': number_of_day, 'name': day_name}
+    var temperatures = []
+    for (var i = 0; i < weatherBitData.data.length; i++) {
+      var day = weatherBitData.data[i]
+      var date = new Date(day.datetime);
+      temperatures.push({
+        'y': day.high_temp,
+        'x': i,
+        'name': weekdays[date.getDay()]
+      })
+    }
+    return temperatures
+  }
 
   return (
     <div class="container fill">
       <h3>Example</h3>
+      <form className="Form" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					autoFocus
+					autoComplete="off"
+					name="search"
+					placeholder="Lausanne"
+				/>
+				<button> Search </button>
+			</form>
       <p>Click on a bar to remove one from the chart.</p>
-        <BarComponent ogData={ogData}/>
+      <div class="container fill">
+        {status === 'idle' && (
+					<div> Let's get started by searching for weather from a place! </div>
+				)}
+				{status === 'error' && <div>{error}</div>}
+				{status === 'fetching' && <div className="loading">Loading.</div>}
+				{status === 'fetched' && console.log(weatherbitToTemperatures(data)) && (
+					<>
+            <BarComponent ogData={weatherbitToTemperatures(data)}/>
+					</>
+				)}
+      </div>
     </div>
   );
 }
