eec4632a93652f885d045731c7bf556c11c04b77
Jonas Blanc <jonas.blanc@epfl.ch>
2023-04-06 23:37:30 +0200

    Remove weather page

--- wa-visualization/src/components/Navigation.js
+++ wa-visualization/src/components/Navigation.js
@@ -1,25 +1,30 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Navigation = () => {
-    return (
-        <>
-            <Navbar bg="dark" variant="dark" expand="sm" fixed='top'>
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img src={require('../../public/logo.png')} width="30" height="30" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav>
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/weather">Weather</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    );
-}
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="sm" fixed="top">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src={require("../../public/logo.png")}
+              width="30"
+              height="30"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 export default Navigation;
+
+//                             <Nav.Link href="/weather">Weather</Nav.Link>
