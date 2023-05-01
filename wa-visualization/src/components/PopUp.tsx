6a439b2c126039ad00d7fbeb762d60cbe98eecef
Tobias Oberdoerfer <tobiaso@ethz.ch>
2023-05-01 20:20:01 +0200

    Custom modals for disclaimer & login QR code

---
+++ wa-visualization/src/components/PopUp.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+// https://react-bootstrap.github.io/components/modal/
+function PopUp({
+  heading,
+  body
+}:any) {  
+    const [show, setShow] = useState(true);
+  
+    const handleClose = () => setShow(false);
+  
+    return (
+      <>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{heading}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className='white-space: pre-line'>{body}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  
+
+export default PopUp;
